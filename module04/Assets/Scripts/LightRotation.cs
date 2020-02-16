5fa3c69aa0f1a18170ade40340bd4d32376c7ebd
Mikhail <gmvbif@gmail.com>
2020-02-16 22:22:28 +0300

    UI сделан просто оверлеем, добавлено движение камеры

---
+++ module04/Assets/Scripts/LightRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+    public float XMax;
+    public float YMax;
+    public float ZMax;
+    public float XShift;
+    public float YShift;
+    public float ZShift;
+
+    public float Speed;
+
+    private Vector3 _rotation;
+    private float _timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotation = transform.rotation.eulerAngles;
+    }
+
+    float RotateOne(float original, float max, float shift)
+    {
+        return original + Mathf.Sin(_timer + shift) * max;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = RotateOne(_rotation.x, XMax, XShift);
+        float y = RotateOne(_rotation.y, YMax, YShift);
+        float z = RotateOne(_rotation.z, ZMax, ZShift);
+
+        transform.rotation = Quaternion.Euler(x, y, z);
+
+        _timer += Time.deltaTime * Speed;
+    }
+}
