df1458b7c31de0c2780502a3a6ab511f813bfb86
Mikhail <gmvbif@gmail.com>
2020-02-17 17:46:30 +0300

    Добавлена загрузка персого уровня

---
+++ module04/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public Slider Slider;
+    public float FakeTime;
+
+    private GameObject _main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        _main = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    public void LoadLevel(string name)
+    {
+        StartCoroutine(LoadLevelAsync(name));
+    }
+
+    private IEnumerator LoadLevelAsync(string name)
+    {
+        _main.SetActive(true);
+
+        var loadResult = SceneManager.LoadSceneAsync(name);
+
+        float timer = 0;
+        loadResult.allowSceneActivation = false;
+        while (loadResult.progress < 0.9f || timer < FakeTime)
+        {
+            Slider.value = Mathf.Min(loadResult.progress, timer / FakeTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        loadResult.allowSceneActivation = true;
+        while (!loadResult.isDone)
+        {
+            Slider.value = loadResult.progress;
+            yield return null;
+        }
+
+        _main.SetActive(false);
+        yield return null;
+    }
+}
