3d74a9d72dd55638331fff9ef0388e76db207580
Mikhail <gmvbif@gmail.com>
2020-03-21 12:11:58 +0300

    Переделан уровень, платформа на несколько точек, простой прыжок

--- module10/Assets/Scripts/Character.cs
+++ module10/Assets/Scripts/Character.cs
@@ -7,6 +7,7 @@ public class Character : MonoBehaviour
 {
     public Transform Visual;
     public float MoveForce;
+    public float JumpForce;
 
     Rigidbody2D rigidBody2D;
     TriggerDetector triggerDetector;
@@ -25,25 +26,50 @@ public class Character : MonoBehaviour
     public void MoveLeft()
     {
         if (triggerDetector.InTrigger)
+        {
             rigidBody2D.AddForce(new Vector2(-MoveForce, 0), ForceMode2D.Force);
+        }
     }
 
     public void MoveRight()
     {
         if (triggerDetector.InTrigger)
+        {
             rigidBody2D.AddForce(new Vector2(MoveForce, 0), ForceMode2D.Force);
+        }
     }
 
-    private void Update()
+    public void Jump()
     {
+        if (triggerDetector.InTrigger)
+        {
+            rigidBody2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Force);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
         float vel = rigidBody2D.velocity.x;
 
         if (vel < -0.01f) {
             visualDirection = -1.0f;
-            Debug.Log($"vel={vel:f5} visualDirection={visualDirection}");
+            //Debug.Log($"vel={vel:f5} visualDirection={visualDirection}");
         } else if (vel > 0.01f) {
             visualDirection = 1.0f;
-            Debug.Log($"vel={vel:f5} visualDirection={visualDirection}");
+            //Debug.Log($"vel={vel:f5} visualDirection={visualDirection}");
         }
 
         Vector3 scale = Visual.localScale;
--- module10/Assets/Scripts/MovingPlatform.cs
+++ module10/Assets/Scripts/MovingPlatform.cs
@@ -4,32 +4,63 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Transform start;
-    public Transform end;
+    public Transform[] path;
     public float speed;
 
     private float current;  // от 0.0 до 1.0
-    private float dir;
+    private float speedInv = -1;
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
         current = 0.0f;
-        dir = 1.0f;
+        index = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        current += dir * speed * Time.deltaTime;
-        if (current > 1.0f) {
-            current = 1.0f;
-            dir = -1.0f;
-        } else if (current < 0.0f) {
+        var start = GetPosition(index - 1);
+        var end = GetPosition(index);
+
+        if (speedInv < 0)
+        {
             current = 0.0f;
-            dir = 1.0f;
+            speedInv = (end - start).magnitude / speed;
         }
 
-        transform.position = Vector3.Lerp(start.position, end.position, current);
+        current += Time.deltaTime / speedInv;
+        if (current > 1.0f) {
+            current = 1.0f;
+
+            index = (index + 1) % path.Length;
+            speedInv = -1;
+        }
+
+        transform.position = Vector3.Lerp(start, end, current);
+    }
+
+    private Vector3 GetPosition(int index)
+    {
+        if (path.Length == 0)
+        {
+            return new Vector3();
+        }
+        else if (path.Length == 1)
+        {
+            return path[0].position;
+        }
+
+        if (index < 0)
+        {
+            index = path.Length - 1;
+        }
+        else if (index >= path.Length)
+        {
+            index = 0;
+        }
+
+        return path[index].position;
     }
 }
